237bd090c9407a09fe0d569480982d28a9cbdf40
Raul Fernandez <raul.fernandez@sbb.ch>
2024-02-22 11:36:18 +0100

    assert added

--- sonar-scanner-maven/maven-basic/src/test/java/com/acme/basic/HelloWorldTest.java
+++ sonar-scanner-maven/maven-basic/src/test/java/com/acme/basic/HelloWorldTest.java
@@ -1,11 +1,15 @@
 package com.acme.basic;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class HelloWorldTest {
 
   @Test
   public void sayHello() {
+
+
     new HelloWorld().sayHello();
+    Assert.assertTrue(true);
   }
 }
